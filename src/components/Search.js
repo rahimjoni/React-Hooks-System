1cc178d2cf0a3c6f39c7c0c5a68b4a13b2817f1f
JONI <arjoni95@gmail.com>
2020-11-30 00:40:31 -0800

    axios request

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,27 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Search(props) {
-    const [term,setTerm] = useState('')
+    const [term,setTerm] = useState('programming')
+    const [result,setResult] = useState([])
 
-    console.log('change value singly')
+    console.log(result)
     useEffect(()=>{
-        console.log('change value from useEffect')
+        const search =async ()=>{
+            const{data} = await axios.get('https://en.wikipedia.org/w/api.php',{
+                params:{
+                    action:'query',
+                    list:'search',
+                    origin:'*',
+                    format:'json',
+                    srsearch:term,
+                }
+            })
+            setResult(data.query.search)
+        }
+
+        search()
+
     },[term])
     return (
         <div>
