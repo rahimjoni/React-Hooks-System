03bbda29b172f734f52fc254b31377ce8d83678d
JONI <arjoni95@gmail.com>
2020-11-29 23:56:13 -0800

    useEffect

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,12 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Search(props) {
     const [term,setTerm] = useState('')
+
+    console.log('change value singly')
+    useEffect(()=>{
+        console.log('change value from useEffect')
+    },[term])
     return (
         <div>
             <div className="ui form">
